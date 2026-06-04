8a328360673659c6ee477d71b49c01578da48d8c
Miru-N <ito.online.shop@gmail.com>
2026-06-03 22:59:06 -0700

    Completed lodash exercises

--- ex05.js
+++ ex05.js
@@ -5,5 +5,5 @@ const _ = require('lodash');
 const numbers = [1, 2, 3, 4];
 
 // your code here
-
+const product = _.reduce(numbers, (total, num) => total * num, 1);
 console.log(product); // Expected output: 24
